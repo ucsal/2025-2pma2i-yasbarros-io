69141f3621e15d5cfd9b3d306847b7e78cbdf3ad
yasbarros-io <yasminrodrigues.barros@ucsal.edu.br>
2025-08-31 16:27:10 -0300

    Final Commit - Build Sucessful

--- src/test/java/br/com/mariojp/solid/isp/DeviceTest.java
+++ src/test/java/br/com/mariojp/solid/isp/DeviceTest.java
@@ -1,7 +1,7 @@
 package br.com.mariojp.solid.isp;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class DeviceTest {
     @Test
